1b963e4d76fa48aff83a18ce2a81a8b1276dec67
Aaron Jacobs <atheriel@gmail.com>
2017-03-19 23:37:45 -0400

    Adds support for bare functions, e.g. %>>=% log.
    Signed-off-by: Aaron Jacobs <atheriel@gmail.com>

--- R/bind_result.R
+++ R/bind_result.R
@@ -7,20 +7,30 @@
     if (typeof(lhs$error) == "NULL") {
         env <- new.env(parent = parent.frame())
         args <- match.call()
+        rhs <- args$rhs
 
-        if (magrittr:::is_first(args$rhs)) {
-            body <- magrittr:::prepare_first(args$rhs)
-            env[["_fn"]] <- eval(call("function", as.pairlist(alist(. = )),
-                                      body), env, env)
-            env[["_lhs"]] <- lhs$result
-            expr <- quote({
-                res <- tryCatch(`_fn`(`_lhs`), error = function(e) e)
-                as_result(res)
-            })
-            eval(expr, env, env)
-        } else {
-            stop("not yet implemented")
         }
+
+        body <- if (magrittr:::is_function(rhs)) {
+            # Turn `fn` into `fn(.)`.
+            magrittr:::prepare_function(rhs)
+        }
+        else if (magrittr:::is_first(rhs)) {
+            # Turn `fn(x, y, z)` into `fn(., x, y, z)`.
+            magrittr:::prepare_first(rhs)
+        }
+        else {
+            stop("Unsupported RHS: ", deparse(rhs), call. = FALSE)
+        }
+
+        env[["_fn"]] <- eval(call("function", as.pairlist(alist(. = )),
+                                  body), env, env)
+        env[["_lhs"]] <- lhs$result
+        expr <- quote({
+            res <- tryCatch(`_fn`(`_lhs`), error = function(e) e)
+            as_result(res)
+        })
+        eval(expr, env, env)
     }
     # Otherwise, skip executing the rhs and simply pass the lhs on.
     else {
