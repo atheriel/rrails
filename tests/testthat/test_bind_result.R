d8204ac28b2bc1defefb15aa47ed2bb53b002d10
Aaron Jacobs <atheriel@gmail.com>
2017-03-19 23:16:45 -0400

    Adds additional tests for basic functionality, some of which fail.
    Signed-off-by: Aaron Jacobs <atheriel@gmail.com>

--- tests/testthat/test_bind_result.R
+++ tests/testthat/test_bind_result.R
@@ -10,3 +10,30 @@ testthat::test_that("bind-result returns a result", {
     # Continuation in the case of errors:
     testthat::expect_is(y %>>=% log(), class = "result")
 })
+
+testthat::test_that("bind-result handles bare functions", {
+    testthat::expect_silent(as_result(10) %>>=% log)
+})
+
+testthat::test_that("bind-result handles functions with explicit dots in first", {
+    # Dots in the function itself.
+    testthat::expect_equal(as_result(10) %>>=% log(.), as_result(log(10)))
+})
+
+testthat::test_that("bind-result handles functions with explicit dots", {
+    testthat::expect_equal(as_result(10) %>>=% log(10, .), as_result(1))
+})
+
+testthat::test_that("bind-result handles functions with operations on dots", {
+    testthat::expect_equal(as_result(10) %>>=% log(. / 2), as_result(log(5)))
+})
+
+testthat::context("non-standard evaluation in bind-result")
+
+testthat::test_that("bind-result env has the correct parent env", {
+    # Override the log function in this environment.
+    log <- sin
+    x <- as_result(10) %>>=% log()
+
+    testthat::expect_equal(x, as_result(sin(10)))
+})
