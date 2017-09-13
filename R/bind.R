6203d462b4b4bb47d71cac9fbf745f8adb2484db
Aaron Jacobs <atheriel@gmail.com>
2017-09-12 21:20:11 -0400

    Improves documentation of operators, now using Roxygen's md support.
    Signed-off-by: Aaron Jacobs <atheriel@gmail.com>

--- R/bind.R
+++ R/bind.R
@@ -1,8 +1,14 @@
 #' The 'bind' operator
 #'
 #' \code{\%>>=\%} (pronounced "bind") is a generic operator intended to match
-#' the semantics of \code{>>=} in other functional programming languages, such
-#' as F# or Haskell.
+#' the semantics of `>>=` in other functional programming languages, such as F#
+#' or Haskell. It is intended that the right-hand side will contain a function
+#' or block, similar to the pipe operator \code{\link[magrittr]{\%>\%}}.
+#'
+#' @param lhs The left-hand side of the operator.
+#' @param rhs Although not required, it is intended that the right-hand side
+#'   contain a function or block, evaluated with `.` bound to the value of
+#'   `lhs`.
 #'
 #' @usage lhs \%>>=\% rhs
 #'
--- R/bind_result.R
+++ R/bind_result.R
@@ -1,4 +1,24 @@
-#' Bind result objects
+#' Safely pipe values through a chain of functions, avoiding errors
+#'
+#' The bind operator for an object of class `"result"` executes the function or
+#' block on the right-hand side if the object contains a value, or passes on the
+#' error it contains if not. If the right-hand side function would raise an
+#' error, that error is captured in the returned value instead. To turn a
+#' regular R object into a result (or vice-versa) see [result].
+#'
+#' @param lhs An object of class `"result"`.
+#' @param rhs A function or block, evaluated with `.` bound to the value of
+#'   `lhs`, or ignored if there is no value in `lhs`.
+#'
+#' @return An object of class `"result"` wrapping the output of the `rhs`
+#'   function or block, or carrying forward its error if it has one.
+#'
+#' @usage lhs \%>>=\% rhs
+#'
+#' @details
+#'
+#' The right-hand side syntax is largely identitcal to that of the pipe
+#' operator, \code{\link[magrittr]{\%>\%}}.
 #'
 #' @rdname bind-result
 #'
