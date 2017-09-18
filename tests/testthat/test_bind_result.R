69ae615c09a249f1841bb866ccdb85bc390a5ff1
Aaron Jacobs <atheriel@gmail.com>
2017-09-18 19:11:05 -0400

    Adds tests for new functionality, plus some tests/context renaming.
    Signed-off-by: Aaron Jacobs <atheriel@gmail.com>

--- tests/testthat/test_bind_result.R
+++ tests/testthat/test_bind_result.R
@@ -1,5 +1,10 @@
 testthat::context("basic bind functionality")
 
+testthat::test_that("bind-default acts like a pipe", {
+  testthat::expect_equal(10 %>>=% log(), log(10))
+  testthat::expect_error("a" %>>=% log())
+})
+
 testthat::test_that("bind-result returns a result", {
   x <- as_result(10) %>>=% log()
   y <- as_result("a") %>>=% log()
@@ -10,6 +15,13 @@ testthat::test_that("bind-result returns a result", {
   # Continuation in the case of errors:
   testthat::expect_is(y %>>=% log(), class = "result")
 })
+
+testthat::test_that("bind-list applies bind to its elements", {
+  testthat::expect_equal(list(a = 1, b = 10, c = 100) %>>=% log(base = 10),
+                         list(a = 0, b = 1, c = 2))
+})
+
+testthat::context("basic right-hand-side syntax parsing")
 
 testthat::test_that("bind-result handles bare functions", {
   testthat::expect_equal(as_result(10) %>>=% log,
@@ -26,21 +38,21 @@ testthat::test_that("bind-result handles explicit dots correctly", {
                          as_result(log(10, 5)))
 })
 
-testthat::context("parentheses and anonymous functions")
+testthat::context("parentheses and anonymous function syntax")
 
-testthat::test_that("bind-result handles parenthesized anonymous functions", {
+testthat::test_that("bind handles parenthesized anonymous functions", {
   testthat::expect_equal(as_result(10) %>>=% (function(x) log(x / 2)),
                          as_result(log(5)))
   testthat::expect_equal(as_result(10) %>>=% (function(x) log(x / 2))(),
                          as_result(log(5)))
 })
 
-testthat::test_that("bind-result allows bare anonymous functions", {
+testthat::test_that("bind allows bare anonymous functions", {
   testthat::expect_equal(as_result(10) %>>=% function(x) log(x / 2),
                          as_result(log(5)))
 })
 
-testthat::test_that("bind-result forbids other parenthesized expressions", {
+testthat::test_that("bind forbids other parenthesized expressions", {
   testthat::expect_error(as_result(10) %>>=% (. + 10))
   testthat::expect_error(as_result(10) %>>=% (
     (function(x) x)
@@ -48,16 +60,16 @@ testthat::test_that("bind-result forbids other parenthesized expressions", {
   testthat::expect_error(as_result(10) %>>=% (. + 10)(100))
 })
 
-testthat::context("blocks")
+testthat::context("block syntax")
 
-testthat::test_that("bind-result handles basic blocks", {
+testthat::test_that("bind handles basic blocks", {
   testthat::expect_equal(as_result(10) %>>=% {
     log(. / 2)
   },
   as_result(log(5)))
 })
 
-testthat::test_that("bind-result handles complex blocks", {
+testthat::test_that("bind handles complex blocks", {
   testthat::expect_equal(as_result(10) %>>=% {
     base <- . * 2
     function() log(., base)
@@ -65,7 +77,25 @@ testthat::test_that("bind-result handles complex blocks", {
   as_result(log(10, 20)))
 })
 
-testthat::context("non-standard evaluation in bind-result")
+testthat::context("testing results and unwrapping")
+
+testthat::test_that("result inspection works as expected", {
+  testthat::expect_true(result_is_ok(as_result(10)))
+  testthat::expect_error(result_is_ok(10))
+})
+
+testthat::test_that("bind-results can be inspected and unwrapped", {
+  x <- list(a = 1, b = 10, c = 100) %>>=% as_result() %>>=% log(base = 10)
+  y <- list(a = "1", b = 10, c = 100) %>>=% as_result() %>>=% log(base = 10)
+
+  testthat::expect_true(result_is_ok(x))
+  testthat::expect_false(result_is_ok(y))
+
+  testthat::expect_equal(unwrap(x), list(a = 0, b = 1, c = 2))
+  testthat::expect_error(unwrap(y))
+})
+
+testthat::context("bind handles non-standard evaluation correctly")
 
 testthat::test_that("bind-result env has the correct parent env", {
   # Override the log function in this environment.
@@ -75,4 +105,23 @@ testthat::test_that("bind-result env has the correct parent env", {
 
   testthat::expect_equal(x, as_result(sin(10)))
   testthat::expect_equal(y, as_result(sin(10)))
+})
+
+testthat::test_that("bind works inside of dplyr verbs", {
+  if (requireNamespace("dplyr", quietly = TRUE)) {
+    df <- dplyr::tibble(x = c(1, 10, 100))
+
+    df <- dplyr::mutate(df, logs = log(x),
+                        safe_logs = as.list(x) %>>=%
+                          as_result() %>>=%
+                          log()
+                        )
+
+    testthat::expect_true(result_is_ok(df$safe_logs))
+
+    df <- dplyr::mutate(df, unwrapped_logs = unwrap(safe_logs),
+                        log_list = as.list(logs))
+
+    testthat::expect_equal(df$log_list, df$unwrapped_logs)
+  }
 })
